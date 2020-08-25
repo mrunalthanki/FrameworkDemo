38028f8ab3a2b6a98c16690c7be5fbf0b9cf904d
Mrunal Thanki <mrunal.thanki@digital.datamatics.com>
2020-08-25 17:34:44 +0530

    Pushing original repository

---
+++ FrameworkDemo/SDKDemoFile.swift
@@ -0,0 +1,15 @@
+//
+//  SDKDemoFile.swift
+//  FrameworkDemo
+//
+//  Created by Mrunal on 25/08/20.
+//  Copyright © 2020 Mrunal. All rights reserved.
+//
+
+import Foundation
+
+public class MySDK {
+    public class func logToConsole(msg: String) {
+        print(msg);
+    }
+}
